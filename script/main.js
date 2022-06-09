e0db353654a886ab2ed0bdcdfdf0c79831578712
mludwig99 <michalludwig99@gmail.com>
2022-06-09 22:28:19 +0200

    38-wyswietlanie-opisu

---
+++ script/main.js
@@ -0,0 +1,5 @@
+let paragraf = document.getElementById('opis');
+let button = document.querySelector('button');
+button.onclick = function() {
+    paragraf.textContent = 'Opis produktu (ang. product description) to tekst znajdujący się w karcie produktu, który wyjaśnia użytkownikom...'
+}
